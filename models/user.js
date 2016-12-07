f17984224012661e0408c7667d695e95abd60776
Pedro Alves <3pedru@gmail.com>
2016-12-07 13:32:14 -0200

    cultivando o TOC

--- models/user.js
+++ models/user.js
@@ -1,13 +1,14 @@
 'use strict';
+
 module.exports = (sequelize, DataTypes) => {
-  return sequelize.define('user', {
-    username: {
-      type: DataTypes.STRING
-    },
-    password: {
-      type: DataTypes.STRING
-    }
-  },{
-      timestamps: false
-  });
+    return sequelize.define('user', {
+        username: {
+            type: DataTypes.STRING
+        },
+        password: {
+            type: DataTypes.STRING
+        }
+    }, {
+        timestamps: false
+    });
 }
--- routes.js
+++ routes.js
@@ -3,49 +3,55 @@
 const User = require('./models').user;
 
 module.exports = (server) => {
-  server.route({
-    method: ['GET','POST'],
-    path: '/',
-    handler: (request, reply) => reply('hello')
-  });
+    server.route({
+        method: ['GET', 'POST'],
+        path: '/',
+        handler: (request, reply) => reply('hello')
+    });
 
-  //Secret route use authentication
-  server.route({
-    method: ['GET', 'POST'],
-    path: '/secret',
-    config:{
-      auth: 'session',
-      handler: (request, reply) => {
-        request.auth.isAuthenticated ? reply('bem vindo') : reply('tchau!');
-      }
-    }
-  })
+    //Secret route use authentication
+    server.route({
+        method: ['GET', 'POST'],
+        path: '/secret',
+        config: {
+            auth: 'session',
+            handler: (request, reply) => {
+                request.auth.isAuthenticated ? reply('bem vindo') : reply('tchau!');
+            }
+        }
+    })
 
-  server.route({
-    method: ['GET','POST'],
-    path: '/logout',
-    handler: (request, reply) => request.cookieAuth.clear()
-  });
+    server.route({
+        method: ['GET', 'POST'],
+        path: '/logout',
+        handler: (request, reply) => request.cookieAuth.clear()
+    });
 
-  server.route({
-    method: 'POST',
-    path: '/login',
-    handler: (request, reply) =>{
-      if(!request.payload){
-        reply("wrong login");
-        return;
-      }
-      let username = request.payload.username;
-      let password = request.payload.password;
-      User.findOne({where:{username:username}})
-        .then((user)=>{
-          if(user && user.get("password") == password){
-            request.cookieAuth.set({ username: username });
-            reply(`Hi, ${username}`);
-          }else{
-            reply('wrong login');
-          }
-        });
-    }
-  });
+    server.route({
+        method: 'POST',
+        path: '/login',
+        handler: (request, reply) => {
+            if (!request.payload) {
+                reply("wrong login");
+                return;
+            }
+            let username = request.payload.username;
+            let password = request.payload.password;
+            User.findOne({
+                    where: {
+                        username: username
+                    }
+                })
+                .then((user) => {
+                    if (user && user.get("password") == password) {
+                        request.cookieAuth.set({
+                            username: username
+                        });
+                        reply(`Hi, ${username}`);
+                    } else {
+                        reply('wrong login');
+                    }
+                });
+        }
+    });
 }
